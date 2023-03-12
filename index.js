b5b4b6ba9c96347d9229fb2079e62e4836953f98
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-03-11 21:08:41 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) { 
+
+}
+function subtract(a, b) { //2
+
+}
+function multiply(a,b) {//3
+
+}
+function divide(a, b) {  //4
+
+}
+function add(a, b) {  //5
+    return a + b;
+}
+function subtract(a, b) {  //6
+  return a-b;
+}
+function multiply(a,b) {  //7
+return a*b;
+}
+function divide(a,b) {   //8
+return a/b;
+}
+function increment(n) { // increment a n and show results 9
+    
+    return n += 1;
+} 
+function decrement(n) {  //10
+    
+    return n -= 1;
+} 
+function makeInt(string) {  //11
+    return parseInt (string,10); 
+    
+}
+function preserveDecimal(n) {
+    return parseFloat (n);
+}
